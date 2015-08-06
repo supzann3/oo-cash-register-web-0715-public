3c4b0103907faa47c6f6f77a2c29608627d412a3
supzann3 <susan.hehe01@gmail.com>
2015-08-05 20:10:21 -0400

    all done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,41 @@
 # Write your code here.
+class CashRegister
+  def initialize(optional_discount=0)
+    @total=0  
+    @optional_discount=optional_discount
+    @title=[]
+  end
+  def discount
+    @optional_discount
+  end
+  def total=(total)
+    @total=total
+  end
+  def total
+    @total
+  end
+  def add_item (title,price,quantity=1)
+    @total+=price*quantity
+    # @title<<title*quantity
+    i=0
+    while i<quantity 
+    @title<<title
+    i+=1
+    end
+  end
+  # require 'pry'
+  def apply_discount
+    if @optional_discount==0
+      return "There is no discount to apply."
+    else
+      @optional_discount=(@optional_discount.to_f/100.to_f)
+      @total=total.to_f*(1.0-@optional_discount.to_f).to_f
+      @total=@total.round
+      # binding.pry
+    return "After the discount, the total comes to $#{@total}."
+    end
+  end
+  def items
+    @title
+  end
+end
